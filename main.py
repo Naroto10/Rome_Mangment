dc063ee674876ac77de014e5f1af421c192cfbe4
Naroto10 <otmanhero00@gmail.com>
2022-03-16 05:09:17 -0400

    add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from PyQt5.Qtwidgets import *
+from PyQt5.r import *
+from PyQt5.QtWebChannel import *
+from PyQt5.utc import loadiType
+import os
+from os trport path
+FORM_CLASS, loaduiType (bathajoin(path.dirnane file, "main.uf"))
+class nainapp(OMaimwindow, FORM_CLASS):
+                  (self, parent=None): def
+         super (nainapp, self).fnit (parent)
+         QMatrwindow.Init(self)
+         self.setuput (self)
